634eab18dfea16712cb7b6845cae70048c582196
cncfanatics <diego.duclos@gmail.com>
2010-06-22 21:50:13 +0200

    Add character and modules, together with getters, setters, adders, removers and iterators

---
+++ types/fit/__init__.py
@@ -0,0 +1,28 @@
+from ..character import Character
+from ..module import Module
+
+class Fit(object):
+    """Represents a fitting, with modules and character"""
+    def __init__(self):
+       self.__modules = []
+       self.__character = None
+
+    @property
+    def character(self):
+        return self.__character;
+    
+    @character.setter
+    def character(self, char):
+        if(type(char) != Character and char != None): raise ValueError("Expecting a character or None, got " .. type(mod))
+        self.__character = char
+        
+    def addModule(self, mod):
+        if(type(mod) != Module): raise ValueError("Expecting a module to be passed, got " .. type(mod))
+        self.__modules.append(mod)
+        
+    def removeModule(self, mod):
+        if(type(mod) != Module): raise ValueError("Expecting a module to be passed, got " .. type(mod))
+        return self.__modules.remove(mod)
+    
+    def iterModules(self):
+        return self.__modules.__iter__()
