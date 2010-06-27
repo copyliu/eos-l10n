aca5ff7704fdad5aa83e5cf36c7efe756fddab03
cncfanatics <diego.duclos@gmail.com>
2010-06-27 22:35:35 +0200

    Move more variables around, get rid of model.datafolder

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import os.path
+
 debug = True
 datafolder = "~/.pyfa/"
+
+def getDataFile(*args):
+    return os.path.expanduser(os.path.join(datafolder,  *args))
+
+gamedata_connectionstring = 'sqlite:///' + getDataFile("eve.db")
+saveddata_connectionstring = 'sqlite:///' + getDataFile("pyfa.db")
--- db/__init__.py
+++ db/__init__.py
@@ -4,13 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from .. import datafolder
 from .. import config
 
-gamedata_engine = create_engine(,  echo = config.debug)
-
+gamedata_engine = create_engine(config.gamedata_connectionstring,  echo = config.debug)
 gamedata_meta = MetaData()
 gamedata_meta.bind = gamedata_engine
-
 gamedata_session = sessionmaker(bind=gamedata_engine)()
 
-#saveddata_engine = create_engine("sqlite:///" + datafolder.getDataFile("saved_data.db"), echo = config.debug)
+saveddata_engine = create_engine(config.saveddata_connectionstring, echo = config.debug)
+gamedata_meta = MetaData()
+gamedata_meta.bind = gamedata_engine
+gamedata_session = sessionmaker(bind=gamedata_engine)()
 
 from gamedata import attribute, category, effect, group, icon, item, marketgroup, metagroup
