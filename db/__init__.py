0f7717263acbbf401c3286063ff245361c411657
cncfanatics <diego.duclos@gmail.com>
2010-06-28 12:34:57 +0200

    Make the gamedata db read-only

--- db/__init__.py
+++ db/__init__.py
@@ -3,10 +3,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .. import config
 
+class ReadOnlyException(Exception):
+    pass
+
+#Method to replace flush with for read-only sessions
+def abort_ro():
+    raise ReadOnlyException()
+
 gamedata_engine = create_engine(config.gamedata_connectionstring,  echo = config.debug)
 gamedata_meta = MetaData()
 gamedata_meta.bind = gamedata_engine
-gamedata_session = sessionmaker(bind=gamedata_engine)()
+gamedata_session = sessionmaker(bind=gamedata_engine, autoflush = False)()
+#Make gamedata_session read only
+gamedata_session.flush = abort_ro
 
 saveddata_engine = create_engine(config.saveddata_connectionstring, echo = config.debug)
 saveddata_meta = MetaData()
