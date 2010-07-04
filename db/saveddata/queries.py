24abce22cea901025f69a682b607cf1875f797fe
cncfanatics <diego.duclos@gmail.com>
2010-07-04 17:10:02 +0200

    Remove more unused imports

--- db/saveddata/queries.py
+++ db/saveddata/queries.py
@@ -1,7 +1,6 @@
 from model.db import saveddata_session
 from model.types import User, Character
 
-from model.db.saveddata.user import users_table
 def getUser(lookfor):
     if type(lookfor) == int: return saveddata_session.query(User).filter(User.ID == lookfor).one()
     elif type(lookfor) == str: return saveddata_session.query(User).filter(User.username == lookfor).one()
