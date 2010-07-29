b7ec8d40f3bb36626c908f4acba70429d6ecc8b3
cncfanatics <diego.duclos@gmail.com>
2010-07-29 23:10:18 +0200

    Fix a small bug (no type)

--- effects/smallProjectileMaxRangeBonus.py
+++ effects/smallProjectileMaxRangeBonus.py
@@ -1,4 +1,5 @@
 #Item: Thrasher [Ship]
+type = "passive"
 def handler(fit, container, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Small Projectile Turret"),
                                   "maxRange", container.getModifiedItemAttr("maxRangeBonus"))
