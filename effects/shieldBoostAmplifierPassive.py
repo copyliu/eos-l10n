abbfaea075b395fd527f24a7a44e082925d9b3d0
DarkPhoenix <phoenix@mail.ru>
2010-07-31 03:30:47 +0400

    Remove unused effect

--- effects/shieldBoostAmplifierPassive.py
+++
@@ -1,5 +0,0 @@
-#Not used by any item
-type = "passive"
-def handler(fit, container, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Operation"),
-                                  "shieldBonus", container.getModifiedItemAttr("shieldBoostMultiplier"))
