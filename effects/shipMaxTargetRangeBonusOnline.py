a08e1e486c62543fe52e98c5642ca905be5226c1
DarkPhoenix <phoenix@mail.ru>
2010-08-01 02:26:32 +0400

    Fix several effects

--- effects/shipMaxTargetRangeBonusOnline.py
+++ effects/shipMaxTargetRangeBonusOnline.py
@@ -2,5 +2,5 @@
 #Modules from group: Signal Amplifier (11 of 11)
 type = "passive"
 def handler(fit, ship, context):
-    fit.ship.increaseItemAttr("maxTargetRange", ship.getModifiedItemAttr("maxTargetRangeBonus"),
+    fit.ship.boostItemAttr("maxTargetRange", ship.getModifiedItemAttr("maxTargetRangeBonus"),
                               stackingPenalties = True)
