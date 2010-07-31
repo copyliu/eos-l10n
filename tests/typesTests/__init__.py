500a5d48101ef8298dc6b6922ac7ff7ead6a4a73
DarkPhoenix <phoenix@mail.ru>
2010-08-01 03:01:29 +0400

    Add calculateBoostMultiplier method and make test to use it

--- tests/typesTests/__init__.py
+++ tests/typesTests/__init__.py
@@ -2,3 +2,12 @@ import math
 #Pass 0 for 1st unpenalized module
 def getStackingPenaltyFactor(x = 1):
     return math.exp(-math.pow(x, 2) / 7.1289)
+
+def calculateBoostMultiplier(boostList, stackingPenalized):
+    boostList = sorted(boostList, reverse = True)
+    multiplier = 1
+    for boostIndex in range(len(boostList)):
+        if stackingPenalized: boost = (boostList[boostIndex]/100)*getStackingPenaltyFactor(boostIndex)
+        else: boost = boostList[boostIndex]/100
+        multiplier *= 1 + boost
+    return multiplier
--- tests/typesTests/interactions/sensorBooster_signalAmplifier.py
+++ tests/typesTests/interactions/sensorBooster_signalAmplifier.py
@@ -1,28 +1,25 @@
 import unittest
 from model import db
 from model.types import Module, Fit, Ship, State
-from model.tests.typesTests import getStackingPenaltyFactor
+from model.tests.typesTests import calculateBoostMultiplier
 
 class TestSensorBoosterSignalAmplifier(unittest.TestCase):
     def setUp(self):
+        self.fit = Fit()
+        self.fit.ship = Ship(db.getItem("Rifter"))
         self.sbi = db.getItem("Sensor Booster II")
         self.sai = db.getItem("Signal Amplifier II")
         self.sbm = Module(self.sbi)
         self.sam = Module(self.sai)
-        self.fit = Fit()
         self.sbm.state = State.ACTIVE
-        self.fit.ship = Ship(db.getItem("Rifter"))
         self.fit.modules.append(self.sbm)
         self.fit.modules.append(self.sam)
         self.fit.calculateModifiedAttributes()
 
     def test_scanRes(self):
         original = self.fit.ship.item.attributes["scanResolution"].value
-        bonusList = []
-        bonusList.append(self.sbi.attributes["scanResolutionBonus"].value/100)
-        bonusList.append(self.sai.attributes["scanResolutionBonus"].value/100)
-        bonusList = sorted(bonusList, reverse = True)
-        expected = original
-        for bonusIndex in range(len(bonusList)):
-            expected *= 1 + bonusList[bonusIndex]*getStackingPenaltyFactor(bonusIndex)
+        boostList = []
+        boostList.append(self.sbi.attributes["scanResolutionBonus"].value)
+        boostList.append(self.sai.attributes["scanResolutionBonus"].value)
+        expected = original * calculateBoostMultiplier(boostList, stackingPenalized = True)
         self.assertAlmostEquals(expected, self.fit.ship.getModifiedItemAttr("scanResolution"), 3)
