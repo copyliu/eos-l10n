c218394d07f2ebe2a1e92e7e2bf4fb37989f6901
cncfanatics <diego.duclos@gmail.com>
2010-08-12 16:45:12 +0200

    Change test code so they can be run from anywhere. Add a path variable in config (uses autodetect by default)

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os.path
+import sys
+
 debug = True
 gamedata_connectionstring = 'sqlite:///' + os.path.expanduser(os.path.join("~", ".pyfa","eve.db"))
 saveddata_connectionstring = 'sqlite:///:memory:'
+
+#Autodetect path, only change if the autodetection bugs out.
+path = os.path.dirname(unicode(__file__, sys.getfilesystemencoding()))
