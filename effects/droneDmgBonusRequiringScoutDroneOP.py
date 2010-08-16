4e2241d05960fee0c3db964b2fe4bf0edb50f633
DarkPhoenix <phoenix@mail.ru>
2010-08-17 00:46:26 +0400

    Attempt to fix effect

--- effects/droneDmgBonusRequiringScoutDroneOP.py
+++ effects/droneDmgBonusRequiringScoutDroneOP.py
@@ -2,5 +2,6 @@
 #Skill: Combat Drone Operation
 type = "passive"
 def handler(fit, skill, context):
-    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Scout Drone Operation"),
-                                 "damageMultiplier", skill.getModifiedItemAttr("damageMultiplierBonus") * skill.level)
+    for damageType in ("em", "explosive", "kinetic", "thermal"):
+        fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Scout Drone Operation"),
+                                     damageType + "Damage", skill.getModifiedItemAttr("damageMultiplierBonus") * skill.level)
