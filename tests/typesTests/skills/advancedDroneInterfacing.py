467cd65ba7e1c7850be9df5463d582c817608d47
DarkPhoenix <phoenix@mail.ru>
2010-08-16 10:54:26 +0400

    Add test for Advanced Drone Interfacing

---
+++ tests/typesTests/skills/advancedDroneInterfacing.py
@@ -0,0 +1,24 @@
+import unittest
+from eos import db
+from eos.types import Fit, Ship, Character, Skill, Module
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestAdvancedDroneInterfacing(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testAdvancedDroneInterfacing")
+        self.adii = db.getItem("Advanced Drone Interfacing")
+        self.adil = 5
+        self.char.addSkill(Skill(self.adii, self.adil))
+        self.dcui = db.getItem("Drone Control Unit I")
+        self.dcum = Module(self.dcui)
+        self.fit.modules.append(self.dcum)
+        self.fit.calculateModifiedAttributes()
+
+    def test_dcuMaxGroupActive(self):
+        self.buildTested = 173608
+        original = self.dcum.item.getAttribute("maxGroupActive")
+        expected = ModifiedAttributeDict()
+        expected.original = self.dcum.item.attributes
+        expected.increase("maxGroupActive", 1 * self.adil)
+        self.assertAlmostEquals(expected["maxGroupActive"], self.dcum.getModifiedItemAttr("maxGroupActive"))
