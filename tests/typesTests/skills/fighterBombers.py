f2b1104bb654f4d2fcc7fb7bf0a5e39e2530e85b
DarkPhoenix <phoenix@mail.ru>
2010-08-18 13:36:29 +0400

    Add test cases for other fighter bombers to FB skill

--- tests/typesTests/skills/fighterBombers.py
+++ tests/typesTests/skills/fighterBombers.py
@@ -11,14 +11,78 @@ class TestFighterBombers(unittest.TestCase):
         self.skillLevel = 5
         self.char.addSkill(Skill(self.skill, self.skillLevel))
         self.fit.character = self.char
+
+    def test_amarr(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Malleus")
+        self.testDrone = Drone(self.testItem)
+        self.testCharge = self.testDrone.charge
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testCharge.attributes
+        for damageType in ("em", "explosive", "kinetic", "thermal"):
+            damageAttribute = damageType + "Damage"
+            skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+            expected.boost(damageAttribute, skillBonus * self.skillLevel)
+            actual = self.testDrone.getModifiedChargeAttr(damageAttribute)
+            self.assertAlmostEquals(expected[damageAttribute], actual)
+
+    def test_caldari(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Mantis")
+        self.testDrone = Drone(self.testItem)
+        self.testCharge = self.testDrone.charge
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testCharge.attributes
+        for damageType in ("em", "explosive", "kinetic", "thermal"):
+            damageAttribute = damageType + "Damage"
+            skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+            expected.boost(damageAttribute, skillBonus * self.skillLevel)
+            actual = self.testDrone.getModifiedChargeAttr(damageAttribute)
+            self.assertAlmostEquals(expected[damageAttribute], actual)
+
+    def test_gallente(self):
+        self.buildTested = 0
         self.testItem = db.getItem("Cyclops")
         self.testDrone = Drone(self.testItem)
         self.testCharge = self.testDrone.charge
         self.fit.drones.append(self.testDrone)
         self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testCharge.attributes
+        for damageType in ("em", "explosive", "kinetic", "thermal"):
+            damageAttribute = damageType + "Damage"
+            skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+            expected.boost(damageAttribute, skillBonus * self.skillLevel)
+            actual = self.testDrone.getModifiedChargeAttr(damageAttribute)
+            self.assertAlmostEquals(expected[damageAttribute], actual)
 
-    def test_fighterBomber(self):
+    def test_minmatar(self):
         self.buildTested = 0
+        self.testItem = db.getItem("Tyrfing")
+        self.testDrone = Drone(self.testItem)
+        self.testCharge = self.testDrone.charge
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testCharge.attributes
+        for damageType in ("em", "explosive", "kinetic", "thermal"):
+            damageAttribute = damageType + "Damage"
+            skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+            expected.boost(damageAttribute, skillBonus * self.skillLevel)
+            actual = self.testDrone.getModifiedChargeAttr(damageAttribute)
+            self.assertAlmostEquals(expected[damageAttribute], actual)
+
+    def test_amarr(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Malleus")
+        self.testDrone = Drone(self.testItem)
+        self.testCharge = self.testDrone.charge
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
         expected = ModifiedAttributeDict()
         expected.original = self.testCharge.attributes
         for damageType in ("em", "explosive", "kinetic", "thermal"):
