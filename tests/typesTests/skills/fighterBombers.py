308dd80d192f8069372389dbb53823fac5c59bfc
DarkPhoenix <phoenix@mail.ru>
2010-08-18 12:59:03 +0400

    Add test for Fighter Bombers skill

---
+++ tests/typesTests/skills/fighterBombers.py
@@ -0,0 +1,29 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Drone
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestFighterBombers(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Fighter Bombers")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+        self.testItem = db.getItem("Cyclops")
+        self.testDrone = Drone(self.testItem)
+        self.testCharge = self.testDrone.charge
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+
+    def test_fighterBomber(self):
+        self.buildTested = 0
+        expected = ModifiedAttributeDict()
+        expected.original = self.testCharge.attributes
+        for damageType in ("em", "explosive", "kinetic", "thermal"):
+            damageAttribute = damageType + "Damage"
+            skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+            expected.boost(damageAttribute, skillBonus * self.skillLevel)
+            actual = self.testDrone.getModifiedChargeAttr(damageAttribute)
+            self.assertAlmostEquals(expected[damageAttribute], actual)
