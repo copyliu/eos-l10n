cef443ca4a496de40f40936dde466fc3020d7831
DarkPhoenix <phoenix@mail.ru>
2010-08-18 13:56:06 +0400

    Add test cases for Gallente Drone Specialization skill

---
+++ tests/typesTests/skills/gallenteDroneSpecialization.py
@@ -0,0 +1,65 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Drone
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestGallenteDroneSpecialization(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Gallente Drone Specialization")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_t2(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Ogre II")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "damageMultiplier"
+        skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_augmented(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("'Augmented' Ogre")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "damageMultiplier"
+        skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_t1(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Ogre I")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "damageMultiplier"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_fighter(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Firbolg")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "damageMultiplier"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
