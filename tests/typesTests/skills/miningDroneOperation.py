baa2757698a3055412cf58bc88f8c0b40b8993f3
DarkPhoenix <phoenix@mail.ru>
2010-08-18 15:19:30 +0400

    Add tests for Mining Drone Operation skill

---
+++ tests/typesTests/skills/miningDroneOperation.py
@@ -0,0 +1,67 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Module, Drone
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestMiningDroneOperation(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Mining Drone Operation")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_civilian(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Civilian Mining Drone")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "miningAmount"
+        skillBonus = self.skill.getAttribute("miningAmountBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_t2(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Mining Drone II")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "miningAmount"
+        skillBonus = self.skill.getAttribute("miningAmountBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_harvester(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Harvester Mining Drone")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "miningAmount"
+        skillBonus = self.skill.getAttribute("miningAmountBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_module(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Miner II")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "miningAmount"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
