d83c24d2ef196bc16730019143fb08d790edef94
DarkPhoenix <phoenix@mail.ru>
2010-08-20 01:57:23 +0400

    Add test for t3 subsystems to electronics skill

--- tests/typesTests/skills/electronics.py
+++ tests/typesTests/skills/electronics.py
@@ -1,25 +1,38 @@
 import unittest
 from eos import db
-from eos.types import Fit, Character, Skill, Ship
+from eos.types import Fit, Character, Skill, Ship, Module
 from eos.modifiedAttributeDict import ModifiedAttributeDict
 
 class TestElectronics(unittest.TestCase):
     def setUp(self):
         self.fit = Fit()
-        self.fit.ship = Ship(db.getItem("Crow"))
         self.char = Character("testSkill")
         self.skill = db.getItem("Electronics")
         self.skillLevel = 5
         self.char.addSkill(Skill(self.skill, self.skillLevel))
         self.fit.character = self.char
-        self.fit.calculateModifiedAttributes()
 
-    def test_cpuOutput(self):
+    def test_cpuOutputShip(self):
         self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Crow"))
+        self.fit.calculateModifiedAttributes()
         targetAttrName = "cpuOutput"
         skillBonus = self.skill.getAttribute("cpuOutputBonus2")
         expected = ModifiedAttributeDict()
         expected.original = self.fit.ship.item.attributes
         expected.boost(targetAttrName, skillBonus * self.skillLevel)
         actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_cpuOutputSubsystem(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Tengu"))
+        self.testItem = db.getItem("Tengu Electronics - CPU Efficiency Gate")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "cpuOutput"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
