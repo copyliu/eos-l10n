616a915d9ae44a475f1ac7cac36532a446f192ee
DarkPhoenix <phoenix@mail.ru>
2010-08-20 01:25:03 +0400

    Add tests for Cynosural Field Theory skill

---
+++ tests/typesTests/skills/cynosuralFieldTheory.py
@@ -0,0 +1,55 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Module
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestCynosuralFieldTheory(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Cynosural Field Theory")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_plain(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Cynosural Field Generator I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "consumptionQuantity"
+        skillBonus = self.skill.getAttribute("consumptionQuantityBonusPercentage")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_covert(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Covert Cynosural Field Generator I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "consumptionQuantity"
+        skillBonus = self.skill.getAttribute("consumptionQuantityBonusPercentage")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    # We're testing it because it also has same target attribute and
+    # consumes same fuel type
+    def test_clone(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Clone Vat Bay I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "consumptionQuantity"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
