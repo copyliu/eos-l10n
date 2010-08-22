d5672a44f3f679216fe6aeeba893f9854c2977a6
DarkPhoenix <phoenix@mail.ru>
2010-08-23 00:33:53 +0400

    Add tests for Signal Dispersion skill

---
+++ tests/typesTests/skills/signalDispersion.py
@@ -0,0 +1,84 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Module, Drone
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestSignalDispersion(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Signal Dispersion")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_ecm(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Estamel's Modified ECM Multispectral Jammer")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        skillBonus = self.skill.getAttribute("scanSkillEwStrengthBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        for scanType in ("Gravimetric", "Radar", "Ladar", "Magnetometric"):
+            targetAttrName = "scan" + scanType + "StrengthBonus"
+            expected.boost(targetAttrName, skillBonus * self.skillLevel)
+            actual = self.testMod.getModifiedItemAttr(targetAttrName)
+            self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_ecmBurst(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("ECM Burst II")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        skillBonus = self.skill.getAttribute("scanSkillEwStrengthBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        for scanType in ("Gravimetric", "Radar", "Ladar", "Magnetometric"):
+            targetAttrName = "scan" + scanType + "StrengthBonus"
+            expected.boost(targetAttrName, skillBonus * self.skillLevel)
+            actual = self.testMod.getModifiedItemAttr(targetAttrName)
+            self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_remoteEcmBurst(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Remote ECM Burst I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        for scanType in ("Gravimetric", "Radar", "Ladar", "Magnetometric"):
+            targetAttrName = "scan" + scanType + "StrengthBonus"
+            actual = self.testMod.getModifiedItemAttr(targetAttrName)
+            self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_ewDrone(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Wasp EC-900")
+        self.testDrone = Drone(self.testItem)
+        self.fit.drones.append(self.testDrone)
+        self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        for scanType in ("Gravimetric", "Radar", "Ladar", "Magnetometric"):
+            targetAttrName = "scan" + scanType + "StrengthBonus"
+            actual = self.testDrone.getModifiedItemAttr(targetAttrName)
+            self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_bomb(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Bomb Launcher I")
+        self.testCharge = db.getItem("Lockbreaker Bomb")
+        self.testMod = Module(self.testItem)
+        self.testMod.charge = self.testCharge
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        expected = ModifiedAttributeDict()
+        expected.original = self.testCharge.attributes
+        for scanType in ("Gravimetric", "Radar", "Ladar", "Magnetometric"):
+            targetAttrName = "scan" + scanType + "StrengthBonus"
+            actual = self.testMod.getModifiedChargeAttr(targetAttrName)
+            self.assertAlmostEquals(expected[targetAttrName], actual)
