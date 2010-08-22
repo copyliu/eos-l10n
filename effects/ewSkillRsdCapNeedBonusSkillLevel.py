f7518534a26626bc9297e8444bcb21c34f17581f
DarkPhoenix <phoenix@mail.ru>
2010-08-22 23:53:19 +0400

    Sensor Linking skill affects remote sensor boosters too

--- effects/ewSkillRsdCapNeedBonusSkillLevel.py
+++ effects/ewSkillRsdCapNeedBonusSkillLevel.py
@@ -3,6 +3,7 @@
 #Skill: Sensor Linking
 type = "passive"
 def handler(fit, container, context):
+    groups = "Remote Sensor Damper", "Remote Sensor Booster"
     level = container.level if "skill" in context else 1
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Remote Sensor Damper",
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name in groups,
                                   "capacitorNeed", container.getModifiedItemAttr("capNeedBonus") * level)
