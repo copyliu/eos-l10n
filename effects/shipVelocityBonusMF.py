53c944d861ca41b810527ed1fb718d74cc7c5ee2
DarkPhoenix <phoenix@mail.ru>
2010-08-22 21:10:06 +0400

    Fix vigil effect to stop Long Range Targeting skill from failing

--- effects/shipVelocityBonusMF.py
+++ effects/shipVelocityBonusMF.py
@@ -2,5 +2,5 @@
 #Ship: Vigil
 type = "passive"
 def handler(fit, ship, context):
-    level = fit.character.getSkill("Minmatar Frigate")
-    fit.ship.boostItemAttr("maxVelocity", ship.getModifiedItemAttr("shipBonusMF") * level)
+    skill = fit.character.getSkill("Minmatar Frigate")
+    fit.ship.boostItemAttr("maxVelocity", ship.getModifiedItemAttr("shipBonusMF") * skill.level)
