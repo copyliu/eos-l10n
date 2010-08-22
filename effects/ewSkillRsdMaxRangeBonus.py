9a724ddd6ff0a0cf102d1be5aee64905887c43cf
DarkPhoenix <phoenix@mail.ru>
2010-08-22 19:36:41 +0400

    Fix Long Distance Jamming's SD effect

--- effects/ewSkillRsdMaxRangeBonus.py
+++ effects/ewSkillRsdMaxRangeBonus.py
@@ -2,7 +2,8 @@
 #Modules named like: Particle Dispersion Projector (6 of 6)
 #Skill: Long Distance Jamming
 type = "passive"
-def handler(fit, module, context):
+def handler(fit, container, context):
+    level = container.level if "skill" in context else 1
     fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Remote Sensor Damper",
-                                  "maxRange", module.getModifiedItemAttr("rangeSkillBonus"),
+                                  "maxRange", container.getModifiedItemAttr("rangeSkillBonus") * level,
                                   stackingPenalties = "skill" not in context)
