461ec7a8de2137fa07a0e9eed1ee31971c2acbf3
DarkPhoenix <phoenix@mail.ru>
2010-08-22 21:45:38 +0400

    Add test for Multitasking skill

---
+++ tests/typesTests/skills/multitasking.py
@@ -0,0 +1,24 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestMultitasking(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Multitasking")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+        self.fit.calculateModifiedAttributes()
+
+    def test_maxTargetsLockedFromSkills(self):
+        self.buildTested = 0
+        targetAttrName = "maxTargetsLockedFromSkills"
+        skillBonus = self.skill.getAttribute("maxTargetBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.fit.EXTRA_ATTRIBUTES
+        expected.increase(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.fit.extraAttributes[targetAttrName]
+        self.assertAlmostEquals(expected[targetAttrName], actual)
