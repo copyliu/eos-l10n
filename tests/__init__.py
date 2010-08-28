498121c99bdf85f7d2cf9008a6e0bae2a059301a
cncfanatics <diego.duclos@gmail.com>
2010-08-28 15:05:31 +0200

    Change license from agpl to lgpl (yes, big change :P)

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,15 +5,15 @@
 # This file and all other files in this folder (and its subfolders) are part of eos.
 #
 # eos is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # eos is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with eos.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
