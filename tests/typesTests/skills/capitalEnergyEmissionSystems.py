6f5234ec67cf6d21682a5a45c6c64aab090f5aaa
DarkPhoenix <phoenix@mail.ru>
2010-08-31 22:38:41 +0400

    Add tests for Capital Energy Emission Systems skill

---
+++ tests/typesTests/skills/capitalEnergyEmissionSystems.py
@@ -0,0 +1,51 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Module
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestCapitalEnergyEmissionSystems(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Capital Energy Emission Systems")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_capacitorNeedCapital(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Capital Energy Transfer Array I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        skillBonus = self.skill.getAttribute("capNeedBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_capacitorNeedNormal(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Medium Energy Transfer Array II")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_capacitorNeedCapitalOther(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Capital Shield Transporter I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
