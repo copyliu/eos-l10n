c0bad962f95d55d466417d31e9bfa2d178ba5eb1
DarkPhoenix <phoenix@mail.ru>
2010-09-01 13:55:57 +0400

    Add tests for Energy Emission Systems skill

---
+++ tests/typesTests/skills/energyEmissionSystems.py
@@ -0,0 +1,68 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Ship, Module
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestEnergyEmissionSystems(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Energy Emission Systems")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_energyNeutralizer(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Medium Unstable Power Fluctuator I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        skillBonus = self.skill.getAttribute("capNeedBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_energyTransfer(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Large Energy Transfer Array II")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        skillBonus = self.skill.getAttribute("capNeedBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_energyTransferCapital(self):
+        self.buildTested = 0
+        self.testItem = db.getItem("Capital Energy Transfer Array I")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        skillBonus = self.skill.getAttribute("capNeedBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_other(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Daredevil"))
+        self.testItem = db.getItem("Catalyzed Cold-Gas I Arcjet Thrusters")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "capacitorNeed"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
