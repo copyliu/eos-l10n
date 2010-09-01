90d3a0fd092ad0f9617841e9e732ebf7b67d8f46
DarkPhoenix <phoenix@mail.ru>
2010-09-02 01:24:07 +0400

    Add ship to the test, as test cannot be actually performed without it

--- tests/typesTests/skills/drones/advancedDroneInterfacing/maxGroupActive.py
+++ tests/typesTests/skills/drones/advancedDroneInterfacing/maxGroupActive.py
@@ -1,39 +1,42 @@
 import unittest
 from eos import db
-from eos.types import Fit, Character, Skill, Module
+from eos.types import Fit, Character, Skill, Ship, Module
 from eos.modifiedAttributeDict import ModifiedAttributeDict
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.skill = db.getItem("Advanced Drone Interfacing")
+        self.testSkill = db.getItem("Advanced Drone Interfacing")
+        self.testShip = db.getItem("Thanatos")
         self.testItem = db.getItem("Drone Control Unit I")
-        self.targetAttrName = "maxGroupActive"
+        self.testAttrName = "maxGroupActive"
         # Define inital setup
         self.initFit = Fit()
         self.initSkillLvl = 1
         self.initChar = Character("initSkills")
-        self.initChar.addSkill(Skill(self.skill, self.initSkillLvl))
+        self.initChar.addSkill(Skill(self.testSkill, self.initSkillLvl))
         self.initFit.character = self.initChar
+        self.initFit.ship = Ship(self.testShip)
         self.initTestMod = Module(self.testItem)
         self.initFit.modules.append(self.initTestMod)
         self.initFit.calculateModifiedAttributes()
-        self.initEos = self.initTestMod.getModifiedItemAttr(self.targetAttrName)
+        self.initEos = self.initTestMod.getModifiedItemAttr(self.testAttrName)
         # Define final setup
         self.finalFit = Fit()
         self.finalSkillLvl = 4
         self.finalChar = Character("finalSkills")
-        self.finalChar.addSkill(Skill(self.skill, self.finalSkillLvl))
+        self.finalChar.addSkill(Skill(self.testSkill, self.finalSkillLvl))
         self.finalFit.character = self.finalChar
+        self.finalFit.ship = Ship(self.testShip)
         self.finalTestMod = Module(self.testItem)
         self.finalFit.modules.append(self.finalTestMod)
         self.finalFit.calculateModifiedAttributes()
-        self.finalEos = self.finalTestMod.getModifiedItemAttr(self.targetAttrName)
+        self.finalEos = self.finalTestMod.getModifiedItemAttr(self.testAttrName)
 
     def test_init_eos_theory(self):
         initTheory = ModifiedAttributeDict()
         initTheory.original = self.testItem.attributes
-        initTheory.increase(self.targetAttrName, 1 * self.initSkillLvl)
-        self.assertEquals(self.initEos, initTheory[self.targetAttrName])
+        initTheory.increase(self.testAttrName, 1 * self.initSkillLvl)
+        self.assertEquals(self.initEos, initTheory[self.testAttrName])
 
     def test_init_eos_ingame(self):
         self.buildTested = 0
@@ -43,8 +46,8 @@ class Test(unittest.TestCase):
     def test_final_eos_theory(self):
         finalTheory = ModifiedAttributeDict()
         finalTheory.original = self.testItem.attributes
-        finalTheory.increase(self.targetAttrName, 1 * self.finalSkillLvl)
-        self.assertEquals(self.finalEos, finalTheory[self.targetAttrName])
+        finalTheory.increase(self.testAttrName, 1 * self.finalSkillLvl)
+        self.assertEquals(self.finalEos, finalTheory[self.testAttrName])
 
     def test_final_eos_ingame(self):
         self.buildTested = 0
