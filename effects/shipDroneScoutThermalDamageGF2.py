46c51013c74905ee2d986f68131b656b598684c2
DarkPhoenix <phoenix@mail.ru>
2010-09-01 20:44:26 +0400

    Add tests for Energy Systems Operation skill

--- effects/shipDroneScoutThermalDamageGF2.py
+++ effects/shipDroneScoutThermalDamageGF2.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Gallente Frigate").level
     fit.drones.filteredItemBoost(lambda mod: mod.item.requiresSkill("Scout Drone Operation"),
-                                 "thermalDamage", ship.getModifiedItemAttr("shipBonusGF2")) * level
+                                 "thermalDamage", ship.getModifiedItemAttr("shipBonusGF2") * level)
--- tests/typesTests/skills/engineering/energySystemsOperation.py
+++ tests/typesTests/skills/engineering/energySystemsOperation.py
@@ -3,71 +3,71 @@ from eos import db
 from eos.types import Fit, Character, Skill, Ship, Module, Drone
 from eos.modifiedAttributeDict import ModifiedAttributeDict
 
-class TestEnergyManagement(unittest.TestCase):
+class TestEnergySystemsOperation(unittest.TestCase):
     def setUp(self):
         self.fit = Fit()
         self.char = Character("testSkill")
-        self.skill = db.getItem("Energy Management")
+        self.skill = db.getItem("Energy Systems Operation")
         self.skillLevel = 5
         self.char.addSkill(Skill(self.skill, self.skillLevel))
         self.fit.character = self.char
 
-    def test_capacitorCapacityShipNormal(self):
+    def test_capacitorRechargeShipNormal(self):
         self.buildTested = 0
-        self.fit.ship = Ship(db.getItem("Crusader"))
+        self.fit.ship = Ship(db.getItem("Helios"))
         self.fit.calculateModifiedAttributes()
-        targetAttrName = "capacitorCapacity"
-        skillBonus = self.skill.getAttribute("capacitorCapacityBonus")
+        targetAttrName = "rechargeRate"
+        skillBonus = self.skill.getAttribute("capRechargeBonus")
         expected = ModifiedAttributeDict()
         expected.original = self.fit.ship.item.attributes
         expected.boost(targetAttrName, skillBonus * self.skillLevel)
         actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
 
-    def test_capacitorCapacityShipCapital(self):
+    def test_capacitorRechargeShipCapital(self):
         self.buildTested = 0
-        self.fit.ship = Ship(db.getItem("Revelation"))
+        self.fit.ship = Ship(db.getItem("Leviathan"))
         self.fit.calculateModifiedAttributes()
-        targetAttrName = "capacitorCapacity"
-        skillBonus = self.skill.getAttribute("capacitorCapacityBonus")
+        targetAttrName = "rechargeRate"
+        skillBonus = self.skill.getAttribute("capRechargeBonus")
         expected = ModifiedAttributeDict()
         expected.original = self.fit.ship.item.attributes
         expected.boost(targetAttrName, skillBonus * self.skillLevel)
         actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
 
-    def test_capacitorCapacityShipCivilian(self):
+    def test_capacitorRechargeShipCivilian(self):
         self.buildTested = 0
-        self.fit.ship = Ship(db.getItem("Impairor"))
+        self.fit.ship = Ship(db.getItem("Reaper"))
         self.fit.calculateModifiedAttributes()
-        targetAttrName = "capacitorCapacity"
-        skillBonus = self.skill.getAttribute("capacitorCapacityBonus")
+        targetAttrName = "rechargeRate"
+        skillBonus = self.skill.getAttribute("capRechargeBonus")
         expected = ModifiedAttributeDict()
         expected.original = self.fit.ship.item.attributes
         expected.boost(targetAttrName, skillBonus * self.skillLevel)
         actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
 
-    def test_capacitorCapacitySubsystem(self):
+    def test_capacitorRechargeSubsystem(self):
         self.buildTested = 0
-        self.fit.ship = Ship(db.getItem("Legion"))
-        self.testItem = db.getItem("Legion Engineering - Power Core Multiplier")
+        self.fit.ship = Ship(db.getItem("Loki"))
+        self.testItem = db.getItem("Loki Engineering - Capacitor Regeneration Matrix")
         self.testMod = Module(self.testItem)
         self.fit.modules.append(self.testMod)
         self.fit.calculateModifiedAttributes()
-        targetAttrName = "capacitorCapacity"
+        targetAttrName = "rechargeRate"
         expected = ModifiedAttributeDict()
         expected.original = self.testItem.attributes
         actual = self.testMod.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
 
-    def test_capacitorCapacityDrone(self):
+    def test_capacitorRechargeDrone(self):
         self.buildTested = 0
-        self.testItem = db.getItem("Curator II")
+        self.testItem = db.getItem("Heavy Armor Maintenance Bot I")
         self.testDrone = Drone(self.testItem)
         self.fit.drones.append(self.testDrone)
         self.fit.calculateModifiedAttributes()
-        targetAttrName = "capacitorCapacity"
+        targetAttrName = "rechargeRate"
         expected = ModifiedAttributeDict()
         expected.original = self.testItem.attributes
         actual = self.testDrone.getModifiedItemAttr(targetAttrName)
