383987ee0020cd24e483050b547341b6b06127bc
DarkPhoenix <phoenix@mail.ru>
2010-09-01 16:45:26 +0400

    Add several tests for basic class of items

--- effects/eliteBonusCommandShipLaserDamageCS1.py
+++ effects/eliteBonusCommandShipLaserDamageCS1.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Command Ships").level
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Medium Energy Turret"),
-                                  "damageMultiplier", ship.getModifiedItemAttr("eliteBonusCoverOps1") * level)
+                                  "damageMultiplier", ship.getModifiedItemAttr("eliteBonusCommandShips1") * level)
--- tests/typesTests/skills/electronicsUpgrades.py
+++ tests/typesTests/skills/electronicsUpgrades.py
@@ -27,6 +27,19 @@ class TestElectronicsUpgrades(unittest.TestCase):
         actual = self.testMod.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
 
+    def test_sensorBackupArrayBasic(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Absolution"))
+        self.testItem = db.getItem("F-42 Reiterative Multi-Frequency Backup Sensors")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "cpu"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
     def test_signalAmplifier(self):
         self.buildTested = 0
         self.fit.ship = Ship(db.getItem("Stiletto"))
@@ -39,6 +52,19 @@ class TestElectronicsUpgrades(unittest.TestCase):
         expected = ModifiedAttributeDict()
         expected.original = self.testItem.attributes
         expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_signalAmplifierBasic(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Ares"))
+        self.testItem = db.getItem("F-90 Positional Signal Amplifier")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "cpu"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
         actual = self.testMod.getModifiedItemAttr(targetAttrName)
         self.assertAlmostEquals(expected[targetAttrName], actual)
 
