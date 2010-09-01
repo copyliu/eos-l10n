5dfb17f6229ba3f604d5c93758d5e6a4e256a91d
DarkPhoenix <phoenix@mail.ru>
2010-09-01 11:29:20 +0400

    Modify ship shield resists using active bonus and nullify passive bonus only when shield hardener is active or overloaded

--- effects/modifyActiveShieldResonanceAndNullifyPassiveResonance.py
+++ effects/modifyActiveShieldResonanceAndNullifyPassiveResonance.py
@@ -1,9 +1,11 @@
 #Used by:
 #Modules from group: Shield Hardener (91 of 91)
+from eos.types import State
 type = "active"
 def handler(fit, module, context):
-    for damageType in ("kinetic", "thermal", "explosive", "em"):
-        fit.ship.boostItemAttr("shield" + damageType.capitalize() + "DamageResonance",
-                               module.getModifiedItemAttr(damageType + "DamageResistanceBonus"),
-                               stackingPenalties = True)
-        module.multiplyItemAttr("passive" + damageType.capitalize() + "DamageResistanceBonus", 0)
+    if module.state >= State.ACTIVE:
+        for damageType in ("kinetic", "thermal", "explosive", "em"):
+            fit.ship.boostItemAttr("shield" + damageType.capitalize() + "DamageResonance",
+                                   module.getModifiedItemAttr(damageType + "DamageResistanceBonus"),
+                                   stackingPenalties = True)
+            module.multiplyItemAttr("passive" + damageType.capitalize() + "DamageResistanceBonus", 0)
