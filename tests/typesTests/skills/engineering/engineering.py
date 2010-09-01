9080bd485044a9b8739649fec7a8ac9cf8183342
DarkPhoenix <phoenix@mail.ru>
2010-09-01 20:49:53 +0400

    Add tests for Engineering skill

---
+++ tests/typesTests/skills/engineering/engineering.py
@@ -0,0 +1,62 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Ship, Module
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class TestEngineering(unittest.TestCase):
+    def setUp(self):
+        self.fit = Fit()
+        self.char = Character("testSkill")
+        self.skill = db.getItem("Engineering")
+        self.skillLevel = 5
+        self.char.addSkill(Skill(self.skill, self.skillLevel))
+        self.fit.character = self.char
+
+    def test_powerOutputShipNormal(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Vengeance"))
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "powerOutput"
+        skillBonus = self.skill.getAttribute("powerEngineeringOutputBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.fit.ship.item.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_powerOutputShipCapital(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Avatar"))
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "powerOutput"
+        skillBonus = self.skill.getAttribute("powerEngineeringOutputBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.fit.ship.item.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_powerOutputShipCivilian(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Velator"))
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "powerOutput"
+        skillBonus = self.skill.getAttribute("powerEngineeringOutputBonus")
+        expected = ModifiedAttributeDict()
+        expected.original = self.fit.ship.item.attributes
+        expected.boost(targetAttrName, skillBonus * self.skillLevel)
+        actual = self.fit.ship.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
+
+    def test_powerOutputSubsystem(self):
+        self.buildTested = 0
+        self.fit.ship = Ship(db.getItem("Proteus"))
+        self.testItem = db.getItem("Proteus Engineering - Supplemental Coolant Injector")
+        self.testMod = Module(self.testItem)
+        self.fit.modules.append(self.testMod)
+        self.fit.calculateModifiedAttributes()
+        targetAttrName = "powerOutput"
+        expected = ModifiedAttributeDict()
+        expected.original = self.testItem.attributes
+        actual = self.testMod.getModifiedItemAttr(targetAttrName)
+        self.assertAlmostEquals(expected[targetAttrName], actual)
