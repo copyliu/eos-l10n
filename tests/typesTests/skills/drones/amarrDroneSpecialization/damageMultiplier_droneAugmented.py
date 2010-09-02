e23be7ca5807d94ad6faf89984b0185449997dc4
DarkPhoenix <phoenix@mail.ru>
2010-09-02 11:43:53 +0400

    Rework test for Amarr Drone Specialization skill

---
+++ tests/typesTests/skills/drones/amarrDroneSpecialization/damageMultiplier_droneAugmented.py
@@ -0,0 +1,58 @@
+import unittest
+from eos import db
+from eos.types import Fit, Character, Skill, Ship, Drone
+from eos.modifiedAttributeDict import ModifiedAttributeDict
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.targetAttrName = "damageMultiplier"
+        self.skill = db.getItem("Amarr Drone Specialization")
+        self.skillBonus = self.skill.getAttribute("damageMultiplierBonus")
+        self.ship = db.getItem("Ishkur")
+        self.item = db.getItem("'Augmented' Infiltrator")
+        # Define inital setup
+        self.iFit = Fit()
+        self.iSkillLvl = 1
+        self.iChar = Character("initSkills")
+        self.iChar.addSkill(Skill(self.skill, self.iSkillLvl))
+        self.iFit.character = self.iChar
+        self.iFit.ship = Ship(self.ship)
+        self.iDrone = Drone(self.item)
+        self.iFit.drones.append(self.iDrone)
+        self.iFit.calculateModifiedAttributes()
+        self.iValEos = self.iDrone.getModifiedItemAttr(self.targetAttrName)
+        # Define final setup
+        self.fFit = Fit()
+        self.fSkillLvl = 4
+        self.fChar = Character("finalSkills")
+        self.fChar.addSkill(Skill(self.skill, self.fSkillLvl))
+        self.fFit.character = self.fChar
+        self.fFit.ship = Ship(self.ship)
+        self.fDrone = Drone(self.item)
+        self.fFit.drones.append(self.fDrone)
+        self.fFit.calculateModifiedAttributes()
+        self.fValEos = self.fDrone.getModifiedItemAttr(self.targetAttrName)
+
+    def test_init_eos_theory(self):
+        # Affected by skill
+        iValTheory = ModifiedAttributeDict()
+        iValTheory.original = self.item.attributes
+        iValTheory.boost(self.targetAttrName, self.skillBonus * self.iSkillLvl)
+        self.assertEquals(self.iValEos, iValTheory[self.targetAttrName])
+
+    def test_final_eos_theory(self):
+        # Affected by skill
+        fTheory = ModifiedAttributeDict()
+        fTheory.original = self.item.attributes
+        fTheory.boost(self.targetAttrName, self.skillBonus * self.fSkillLvl)
+        self.assertEquals(self.fValEos, fTheory[self.targetAttrName])
+
+    def test_diff_eos_ingame(self):
+        # Use relative damage boost as we cannot see modified
+        # drone damage multiplier ingame
+        self.buildTested = 0
+        iValIngame = 102.0
+        fValIngame = 108.0
+        dValIngame = fValIngame/iValIngame
+        dValEos = self.fValEos/self.iValEos
+        self.assertEquals(dValEos, dValIngame)
