e738d6e078f433f1c49ffa62467d31d8050cf28c
cncfanatics <diego.duclos@gmail.com>
2010-09-03 14:42:21 +0200

    Fix bug described in ticket #9

--- effects/shipDronesMaxGC2.py
+++ effects/shipDronesMaxGC2.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Gallente Cruiser").level
     amount = ship.getModifiedItemAttr("shipBonusGC2")
-    fit.extraAttributes["maxActiveDrones"].increase(amount * level)
+    fit.extraAttributes.increase("maxActiveDrones", amount * level)
