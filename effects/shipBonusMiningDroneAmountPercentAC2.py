ed06639f593bfd4ec1e71e4c1c964f15d325f5df
cncfanatics <diego.duclos@gmail.com>
2010-09-03 14:31:49 +0200

    Fix the crash with the arby described in ticket #8

--- effects/shipBonusMiningDroneAmountPercentAC2.py
+++ effects/shipBonusMiningDroneAmountPercentAC2.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Amarr Cruiser").level
     fit.drones.filteredItemBoost(lambda drone: drone.item.group.name == "Combat Drone",
-                                 "miningAmount", ship.Mining("shipBonusAC2") * level)
+                                 "miningAmount", ship.getModifiedItemAttr("shipBonusAC2") * level)
