726096a04dd2469114ef6849f50e81e0e7d7cb7f
cncfanatics <diego.duclos@gmail.com>
2010-09-03 15:09:40 +0200

    Fix the bug with vexors described in ticket #12

--- effects/shipBonusMiningDroneAmountPercentGC2.py
+++ effects/shipBonusMiningDroneAmountPercentGC2.py
@@ -5,4 +5,4 @@ type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Gallente Cruiser").level
     fit.drones.filteredItemBoost(lambda drone: drone.item.group.name == "Mining Drone",
-                                 "miningAmount", ship.Mining("shipBonusGC2") * level)
+                                 "miningAmount", ship.getModifiedItemAttr("shipBonusGC2") * level)
