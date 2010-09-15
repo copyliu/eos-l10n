ce016b0cb69029beac85133f2bc25d7e74e06692
cncfanatics <diego.duclos@gmail.com>
2010-09-15 17:07:43 +0200

    Some T3 related fixes

--- effects/shieldRechargeRateAddPassive.py
+++ effects/shieldRechargeRateAddPassive.py
@@ -2,4 +2,4 @@
 #Subsystems from group: Defensive Systems (16 of 16)
 type = "passive"
 def handler(fit, module, context):
-    fit.ship.increaseItemAttr("shieldRechargeRate", module.getModifiedItemAtt("shieldRechargeRate"))
+    fit.ship.increaseItemAttr("shieldRechargeRate", module.getModifiedItemAttr("shieldRechargeRate") or 0)
