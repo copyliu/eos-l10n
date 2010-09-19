70dba488f73339989d6b69a18e8d5e193760e44a
cncfanatics <diego.duclos@gmail.com>
2010-09-19 10:16:56 +0200

    Scope the gamedata session so it can be used from multiple threads

--- db/__init__.py
+++ db/__init__.py
@@ -18,7 +18,7 @@
 #===============================================================================
 
 from sqlalchemy import MetaData,  create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import pool
 from eos import config
@@ -30,7 +30,7 @@ gamedata_engine = create_engine(config.gamedata_connectionstring,
                                 echo = config.debug)
 gamedata_meta = MetaData()
 gamedata_meta.bind = gamedata_engine
-gamedata_session = sessionmaker(bind=gamedata_engine, autoflush=False)()
+gamedata_session = scoped_session(sessionmaker(bind=gamedata_engine, autoflush=False))
 
 if config.saveddata_connectionstring is not None:
     saveddata_engine = create_engine(config.saveddata_connectionstring, echo=config.debug, poolclass=pool.StaticPool)
