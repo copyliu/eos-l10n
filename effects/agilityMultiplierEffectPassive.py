94e39a4ff420537f4eb31b94b43a76bdcd7c6086
DarkPhoenix <phoenix@mail.ru>
2010-09-26 02:46:59 +0400

    Fix couple of failing effects

--- effects/agilityMultiplierEffectPassive.py
+++ effects/agilityMultiplierEffectPassive.py
@@ -2,4 +2,4 @@
 #Modules named like: Polycarbon Engine Housing (6 of 6)
 type = "passive"
 def handler(fit, module, context):
-    fit.ship.boostItemAttr("agility", module.getItemAttr("agilityMultiplier"), stackingPenalties = True)
+    fit.ship.boostItemAttr("agility", module.getModifiedItemAttr("agilityMultiplier"), stackingPenalties = True)
--- effects/capNeedBonusEffectLasers.py
+++ effects/capNeedBonusEffectLasers.py
@@ -2,5 +2,5 @@
 #Modules named like: Energy Discharge Elutriation (6 of 6)
 type = "passive"
 def handler(fit, module, context):
-    fit.modules.filteredItemBoostr(lambda mod: mod.item.group.name == "Energy Weapon",
-                                   "capacitorNeed", module.getModifiedItemAttr("capNeedBonus"))
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Energy Weapon",
+                                  "capacitorNeed", module.getModifiedItemAttr("capNeedBonus"))
