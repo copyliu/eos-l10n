2b4518836255fcdd75714ea53045d363f4fbfc43
DarkPhoenix <phoenix@mail.ru>
2010-10-05 22:55:55 +0400

    Fix more advanced ammo like Barrage

--- effects/ammoInfluenceCapNeed.py
+++ effects/ammoInfluenceCapNeed.py
@@ -2,4 +2,4 @@
 #Items from category: Charge (461 of 814)
 type = "passive"
 def handler(fit, module, context):
-    module.boostItemAttr("capacitorNeed", module.getModifiedChargeAttr("capNeedBonus"))
+    module.boostItemAttr("capacitorNeed", module.getModifiedChargeAttr("capNeedBonus") or 0)
