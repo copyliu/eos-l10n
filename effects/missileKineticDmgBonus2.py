df2702190ae1071a96183bec5b5db94bc27a505f
DarkPhoenix <phoenix@mail.ru>
2010-10-06 00:56:22 +0400

    Add couple test sets for missile skills and fix bugged missile dmg bonus effects

--- effects/missileKineticDmgBonus2.py
+++ effects/missileKineticDmgBonus2.py
@@ -3,6 +3,6 @@
 #Skill: Rockets
 #Skill: Torpedoes
 type = "passive"
-def handler(fit, container, context):
-    fit.modules.filteredChargeMultiply(lambda mod: mod.charge.requiresSkill("Missile Launcher Operation"),
-                                       "kineticDamage", container.getModifiedItemAttr("damageMultiplierBonus"))
+def handler(fit, skill, context):
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill(skill),
+                                    "kineticDamage", skill.getModifiedItemAttr("damageMultiplierBonus") * skill.level)
--- tests/typesTests/skills/drones/fighterBombers.py
+++ tests/typesTests/skills/drones/fighterBombers.py
@@ -19,48 +19,6 @@ class Test(TestBase):
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
 
-    def test_explosiveDamage_droneFighterBomber(self):
-        self.buildTested = 0
-        attr = "explosiveDamage"
-        item = "Tyrfing"
-        iLvl = 1
-        iIngame = 1.2
-        fLvl = 4
-        fIngame = 1.8
-        iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, getCharge=True)
-        fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, getCharge=True)
-        dIngame = fIngame / iIngame
-        dEos = fEos / iEos
-        self.assertAlmostEquals(dEos, dIngame)
-
-    def test_kineticDamage_droneFighterBomber(self):
-        self.buildTested = 0
-        attr = "kineticDamage"
-        item = "Mantis"
-        iLvl = 1
-        iIngame = 1.2
-        fLvl = 4
-        fIngame = 1.8
-        iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, getCharge=True)
-        fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, getCharge=True)
-        dIngame = fIngame / iIngame
-        dEos = fEos / iEos
-        self.assertAlmostEquals(dEos, dIngame)
-
-    def test_thermalDamage_droneFighterBomber(self):
-        self.buildTested = 0
-        attr = "thermalDamage"
-        item = "Cyclops"
-        iLvl = 1
-        iIngame = 1.2
-        fLvl = 4
-        fIngame = 1.8
-        iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, getCharge=True)
-        fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, getCharge=True)
-        dIngame = fIngame / iIngame
-        dEos = fEos / iEos
-        self.assertAlmostEquals(dEos, dIngame)
-
     def test_emDamage_other(self):
         self.buildTested = 0
         attr = "emDamage"
@@ -72,6 +30,20 @@ class Test(TestBase):
         fIngame = 1.0
         iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, cont=cont)
         fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, cont=cont)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_explosiveDamage_droneFighterBomber(self):
+        self.buildTested = 0
+        attr = "explosiveDamage"
+        item = "Tyrfing"
+        iLvl = 1
+        iIngame = 1.2
+        fLvl = 4
+        fIngame = 1.8
+        iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, getCharge=True)
+        fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, getCharge=True)
         dIngame = fIngame / iIngame
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
@@ -91,6 +63,20 @@ class Test(TestBase):
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
 
+    def test_kineticDamage_droneFighterBomber(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Mantis"
+        iLvl = 1
+        iIngame = 1.2
+        fLvl = 4
+        fIngame = 1.8
+        iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, getCharge=True)
+        fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, getCharge=True)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
     def test_kineticDamage_other(self):
         self.buildTested = 0
         attr = "kineticDamage"
@@ -102,6 +88,20 @@ class Test(TestBase):
         fIngame = 1.0
         iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, cont=cont)
         fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, cont=cont)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_thermalDamage_droneFighterBomber(self):
+        self.buildTested = 0
+        attr = "thermalDamage"
+        item = "Cyclops"
+        iLvl = 1
+        iIngame = 1.2
+        fLvl = 4
+        fIngame = 1.8
+        iEos = self.skillTestGetItemAttr(self.skill, iLvl, item, attr, getCharge=True)
+        fEos = self.skillTestGetItemAttr(self.skill, fLvl, item, attr, getCharge=True)
         dIngame = fIngame / iIngame
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
