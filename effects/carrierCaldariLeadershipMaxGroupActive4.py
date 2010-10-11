f21cddf3a17eafc72fe15107ebda813deee796e7
DarkPhoenix <phoenix@mail.ru>
2010-10-12 00:55:31 +0400

    Fix couple of carrier effects

--- effects/carrierCaldariLeadershipMaxGroupActive4.py
+++ effects/carrierCaldariLeadershipMaxGroupActive4.py
@@ -2,6 +2,6 @@
 #Ship: Wyvern
 type = "active"
 def handler(fit, ship, context):
-    level = fit.character.getSkill("Caldari Carrier")
+    level = fit.character.getSkill("Caldari Carrier").level
     fit.modules.filteredItemIncrease(lambda mod: mod.item.group.name == "Gang Coordinator",
                                   "maxGroupActive", ship.getModifiedItemAttr("carrierCaldariBonus4") * level)
