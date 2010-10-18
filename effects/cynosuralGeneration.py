88ba3ebf7b921dee462af1a05410973190a216af
cncfanatics <diego.duclos@gmail.com>
2010-10-18 10:26:54 +0200

    Typo fix

--- effects/cynosuralGeneration.py
+++ effects/cynosuralGeneration.py
@@ -2,4 +2,4 @@
 #Modules from group: Cynosural Field (2 of 2)
 type = "active"
 def handler(fit, module, context):
-    fit.ship.boostItemAttr("maxVelocity", module.getModifidItemAttr("speedFactor"))
+    fit.ship.boostItemAttr("maxVelocity", module.getModifiedItemAttr("speedFactor"))
