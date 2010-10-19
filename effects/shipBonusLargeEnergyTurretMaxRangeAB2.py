ae14fd775f73bf73bc0aacdcb1042b144a32505c
DarkPhoenix <phoenix@mail.ru>
2010-10-20 00:15:43 +0400

    Fix #131 (Ship bonuses not being correctly applied)

--- effects/shipBonusLargeEnergyTurretMaxRangeAB2.py
+++ effects/shipBonusLargeEnergyTurretMaxRangeAB2.py
@@ -4,5 +4,5 @@
 type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Amarr Battleship").level
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Large Energy Turret"),
-                                    "maxRange", ship.getModifiedItemAttr("shipBonusAB2") * level)
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Large Energy Turret"),
+                                  "maxRange", ship.getModifiedItemAttr("shipBonusAB2") * level)
