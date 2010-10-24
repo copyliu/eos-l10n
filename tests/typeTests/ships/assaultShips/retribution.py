c31b7199b92a652454ba2ad707dc0bcc42471abd
DarkPhoenix <phoenix@mail.ru>
2010-10-24 23:42:29 +0400

    Add amarr AF tests

---
+++ tests/typeTests/ships/assaultShips/retribution.py
@@ -0,0 +1,96 @@
+from eos.tests import TestBase
+
+class Test(TestBase):
+    def setUp(self):
+        TestBase.setUp(self)
+        self.ship = "Retribution"
+
+    def test_amarrFrigate_capacitorNeed_moduleEnergyWeaponSkillrqSmallEnergyTurret(self):
+        self.buildTested = 0
+        attr = "capacitorNeed"
+        item = "Medium Pulse Laser I"
+        skill = "Amarr Frigate"
+        iLvl = 1
+        iIngame = 0.9
+        fLvl = 4
+        fIngame = 0.6
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_amarrFrigate_capacitorNeed_moduleEnergyWeaponNoSkillrqSmallEnergyTurret(self):
+        self.buildTested = 0
+        attr = "capacitorNeed"
+        item = "Focused Medium Pulse Laser I"
+        skill = "Amarr Frigate"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_assaultShips_maxRange_moduleEnergyWeaponSkillrqSmallEnergyTurret(self):
+        self.buildTested = 0
+        attr = "maxRange"
+        item = "Medium Beam Laser I"
+        skill = "Assault Ships"
+        iLvl = 1
+        iIngame = 1.1
+        fLvl = 4
+        fIngame = 1.4
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_assaultShips_maxRange_moduleEnergyWeaponNoSkillrqSmallEnergyTurret(self):
+        self.buildTested = 0
+        attr = "maxRange"
+        item = "Heavy Beam Laser I"
+        skill = "Assault Ships"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_assaultShips_damageMultiplier_moduleEnergyWeaponSkillrqSmallEnergyTurret(self):
+        self.buildTested = 0
+        attr = "damageMultiplier"
+        item = "Gatling Pulse Laser I"
+        skill = "Assault Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_assaultShips_damageMultiplier_moduleEnergyWeaponNoSkillrqSmallEnergyTurret(self):
+        self.buildTested = 0
+        attr = "damageMultiplier"
+        item = "Focused Medium Pulse Laser I"
+        skill = "Assault Ships"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
