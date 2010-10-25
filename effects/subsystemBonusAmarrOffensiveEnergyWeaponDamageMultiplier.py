46d0bc022bbaaa0a6e9c4cbee1787be39a75f4af
Corollax <corollax@gmail.com>
2010-10-25 01:57:53 -0500

    Fixed indentation on the amarr offensive subsystem.
    cnc deserves to be picky :)

--- effects/subsystemBonusAmarrOffensiveEnergyWeaponDamageMultiplier.py
+++ effects/subsystemBonusAmarrOffensiveEnergyWeaponDamageMultiplier.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, module, context):
     level = fit.character.getSkill("Amarr Offensive Systems").level
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Medium Energy Turret"),
-                                    "damageMultiplier", module.getModifiedItemAttr("subsystemBonusAmarrOffensive") * level)
+        "damageMultiplier", module.getModifiedItemAttr("subsystemBonusAmarrOffensive") * level)
