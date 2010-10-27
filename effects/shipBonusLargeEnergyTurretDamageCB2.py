fd36fc8c770bb08d38d1b424c23794787c714138
Corollax <corollax@gmail.com>
2010-10-27 18:17:17 -0500

    Fix missing Nightmare +5%/level damage bonus
    Just copy-pasta'd from the Paladin, really.

--- effects/shipBonusLargeEnergyTurretDamageCB2.py
+++ effects/shipBonusLargeEnergyTurretDamageCB2.py
@@ -3,5 +3,5 @@
 type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Caldari Battleship").level
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Large Energy Turret"),
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Large Energy Turret"),
                                     "damageMultiplier", ship.getModifiedItemAttr("shipBonus2CB") * level)
