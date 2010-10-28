a57531436a0ba01163d5865316f92fe51e424d7a
Corollax <corollax@gmail.com>
2010-10-27 19:04:05 -0500

    Fix Daredevil +200% role DPS bonus
    Simple fix, I'm pushing it. I don't think cnc will mind on something this trivial.

--- effects/shipBonusPirateSmallHybridDmg.py
+++ effects/shipBonusPirateSmallHybridDmg.py
@@ -2,5 +2,5 @@
 #Ship: Daredevil
 type = "passive"
 def handler(fit, ship, context):
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Small Hybrid Turret"),
-                                    "damageMultiplier", ship.getModifiedItemAttr("shipBonusPirateFaction"))
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Small Hybrid Turret"),
+                                  "damageMultiplier", ship.getModifiedItemAttr("shipBonusPirateFaction"))
