b8ad6a510ee770b07736da72265b719f50c067c8
DarkPhoenix <phoenix@mail.ru>
2010-10-29 01:04:59 +0400

    Fix HG Snake Omega

--- effects/navigationVelocityBonusPostPercentMaxVelocityShip.py
+++ effects/navigationVelocityBonusPostPercentMaxVelocityShip.py
@@ -5,5 +5,6 @@
 type = "passive"
 def handler(fit, container, context):
     level = container.level if "skill" in context else 1
-    fit.ship.boostItemAttr("maxVelocity", container.getModifiedItemAttr("velocityBonus") * level,
+    amount = container.getModifiedItemAttr("velocityBonus") or 0
+    fit.ship.boostItemAttr("maxVelocity", amount * level,
                            stackingPenalties = "skill" not in context and "implant" not in context)
