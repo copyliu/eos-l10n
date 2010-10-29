d305c3cb417417f2f81a662b6d981f1d8810e53b
DarkPhoenix <phoenix@mail.ru>
2010-10-30 01:23:15 +0400

    Add check for disallow assistance to ET arrays

--- effects/energyTransfer.py
+++ effects/energyTransfer.py
@@ -2,7 +2,7 @@
 #Modules from group: Energy Transfer Array (37 of 37)
 type = "projected", "active"
 def handler(fit, module, context):
-    if "projected" not in context: return
-    amount = module.getModifiedItemAttr("powerTransferAmount")
-    duration = module.getModifiedItemAttr("duration") / 1000.0
-    fit.addDrain(duration, -amount, 0)
+    if "projected" in context and not fit.ship.getModifiedItemAttr("disallowAssistance") == 1:
+        amount = module.getModifiedItemAttr("powerTransferAmount")
+        duration = module.getModifiedItemAttr("duration") / 1000.0
+        fit.addDrain(duration, -amount, 0)
