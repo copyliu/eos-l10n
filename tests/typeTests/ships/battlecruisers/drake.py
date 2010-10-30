d7e771508eb73dba48250bb03ed0c9dc7b24ca80
DarkPhoenix <phoenix@mail.ru>
2010-10-30 16:14:14 +0400

    Add tests for DRAKE THE UNNAMABLE

---
+++ tests/typeTests/ships/battlecruisers/drake.py
@@ -0,0 +1,238 @@
+from eos.tests import TestBase
+
+class Test(TestBase):
+    def setUp(self):
+        TestBase.setUp(self)
+        self.ship = "Drake"
+
+    def test_battlecruisers_shieldEmDamageResonance_ship(self):
+        self.buildTested = 0
+        attr = "shieldEmDamageResonance"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 0.95
+        fLvl = 4
+        fIngame = 0.8
+        iEos = self.getShipAttr(attr, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getShipAttr(attr, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldEmDamageResonance_other(self):
+        self.buildTested = 0
+        attr = "shieldEmDamageResonance"
+        item = "Damage Control I"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldExplosiveDamageResonance_ship(self):
+        self.buildTested = 0
+        attr = "shieldExplosiveDamageResonance"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 0.95
+        fLvl = 4
+        fIngame = 0.8
+        iEos = self.getShipAttr(attr, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getShipAttr(attr, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldExplosiveDamageResonance_other(self):
+        self.buildTested = 0
+        attr = "shieldExplosiveDamageResonance"
+        item = "Damage Control I"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldKineticDamageResonance_ship(self):
+        self.buildTested = 0
+        attr = "shieldKineticDamageResonance"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 0.95
+        fLvl = 4
+        fIngame = 0.8
+        iEos = self.getShipAttr(attr, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getShipAttr(attr, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldKineticDamageResonance_other(self):
+        self.buildTested = 0
+        attr = "shieldKineticDamageResonance"
+        item = "Damage Control I"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldThermalDamageResonance_ship(self):
+        self.buildTested = 0
+        attr = "shieldThermalDamageResonance"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 0.95
+        fLvl = 4
+        fIngame = 0.8
+        iEos = self.getShipAttr(attr, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getShipAttr(attr, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_shieldThermalDamageResonance_other(self):
+        self.buildTested = 0
+        attr = "shieldThermalDamageResonance"
+        item = "Damage Control I"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_kineticDamage_chargeMissileAssault(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Terror Assault Missile"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_kineticDamage_chargeMissileAssaultAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Terror Rage Assault Missile"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_kineticDamage_chargeMissileHeavy(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Scourge Heavy Missile"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_kineticDamage_chargeMissileHeavyAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Scourge Precision Heavy Missile"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_kineticDamage_chargeMissileHeavyFof(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Hydra F.O.F. Heavy Missile I"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_battlecruisers_kineticDamage_chargeOther(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Bloodclaw Light Missile"
+        skill = "Battlecruisers"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_role_cpu_moduleGangCoordinatorSkillrqLeadership(self):
+        self.buildTested = 0
+        attr = "cpu"
+        item = "Siege Warfare Link - Active Shielding"
+        ship_other = "Abaddon"
+        iIngame = 1.0
+        fIngame = 0.01
+        iEos = self.getItemAttr(attr, item, ship=ship_other)
+        fEos = self.getItemAttr(attr, item, ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_role_cpu_moduleGangCoordinatorNoSkillrqLeadership(self):
+        self.buildTested = 0
+        attr = "cpu"
+        item = "Command Processor I"
+        ship_other = "Abaddon"
+        iIngame = 1.0
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, ship=ship_other)
+        fEos = self.getItemAttr(attr, item, ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
