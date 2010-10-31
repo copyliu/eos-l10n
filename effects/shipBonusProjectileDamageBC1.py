86b01bf1fc0da710c6ff1a9674489cd509e57de6
DarkPhoenix <phoenix@mail.ru>
2010-10-31 13:35:30 +0300

    Finish BC tests

--- effects/shipBonusProjectileDamageBC1.py
+++ effects/shipBonusProjectileDamageBC1.py
@@ -3,5 +3,5 @@
 type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Battlecruisers").level
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Medium Projectile Turret"),
-                                    "damageMultiplier", ship.getModifiedItemAttr("shipBonusBC1") * level)
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Medium Projectile Turret"),
+                                  "damageMultiplier", ship.getModifiedItemAttr("shipBonusBC1") * level)
