4ddd7015561a6032039171dd20085f9018b72b05
Corollax <corollax@gmail.com>
2010-11-01 11:00:23 -0500

    Added neuts/nos to maxRange calculation
    GUI automatically picks up the appropriate values now. Nice architecture is nice.

--- saveddata/module.py
+++ saveddata/module.py
@@ -151,7 +151,7 @@ class Module(HandledItem, HandledCharge, ItemAttrShortcut, ChargeAttrShortcut):
 
     @property
     def maxRange(self):
-        attrs = ("maxRange", "empFieldRange", "ecmBurstRange", "powerTransferRange", "shieldTransferRange")
+        attrs = ("maxRange", "empFieldRange", "ecmBurstRange", "powerTransferRange", "shieldTransferRange", "energyDestabilizationRange")
         for attr in attrs:
             maxRange = self.getModifiedItemAttr(attr)
             if maxRange is not None: return maxRange
