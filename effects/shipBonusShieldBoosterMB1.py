1d1c26c53775e688dbf1dae3424e3c60444ab7f9
DarkPhoenix <phoenix@mail.ru>
2010-11-04 01:32:54 +0300

    Add matari BS tests

--- effects/shipBonusShieldBoosterMB1.py
+++ effects/shipBonusShieldBoosterMB1.py
@@ -3,5 +3,5 @@
 type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Minmatar Battleship").level
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.group.name == "Shield Booster",
-                                    "shieldBonus", ship.getModifiedItemAttr("shipBonusMB") * level)
+    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Shield Booster",
+                                  "shieldBonus", ship.getModifiedItemAttr("shipBonusMB") * level)
--- effects/shipLauncherRofBonusMB1.py
+++ effects/shipLauncherRofBonusMB1.py
@@ -2,7 +2,7 @@
 #Ships named like: Typhoon (2 of 2)
 type = "passive"
 def handler(fit, ship, context):
-    level = fit.character.getSkill("Destroyers").level
+    level = fit.character.getSkill("Minmatar Battleship").level
     groups = ("Missile Launcher Siege", "Missile Launcher Cruise")
     fit.modules.filteredItemBoost(lambda mod: mod.item.group.name in groups,
                                   "speed", ship.getModifiedItemAttr("shipBonusMB") * level)
