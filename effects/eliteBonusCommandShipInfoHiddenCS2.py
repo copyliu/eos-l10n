3dce8fb358c749620b293d8fdf9cd24b40e10be5
DarkPhoenix <phoenix@mail.ru>
2010-11-04 23:37:15 +0300

    Finish CS tests

--- effects/eliteBonusCommandShipInfoHiddenCS2.py
+++ effects/eliteBonusCommandShipInfoHiddenCS2.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Command Ships").level
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Information Warfare Specialist"),
-                                  "commandBonus", ship.getModifiedItemAttr("eliteBonusCommandShips2") * level)
+                                  "commandBonusHidden", ship.getModifiedItemAttr("eliteBonusCommandShips2") * level)
