7b52b88e57774e9cd5144ddcef7584559fbc1959
DarkPhoenix <phoenix@mail.ru>
2010-11-05 12:58:23 +0300

    Get rid of covops cloak cpu bonus hardcoding, fixes #67

---
+++ effects/zColinTransportsCloakCpuBonus.py
@@ -0,0 +1,8 @@
+#Used by:
+#Ships from group: Transport Ship (4 of 8)
+type = "passive"
+runTime = "early"
+def handler(fit, ship, context):
+    level = fit.character.getSkill("Transport Ships").level
+    bonus = ship.getModifiedItemAttr("eliteIndustrialCovertCloakBonus")
+    fit.ship.boostItemAttr("cloakingCpuNeedBonus", bonus * level)
