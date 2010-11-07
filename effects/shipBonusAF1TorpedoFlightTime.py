74f0b7e6fd3728eff7c0b39759a2b8100001ef7d
DarkPhoenix <phoenix@mail.ru>
2010-11-07 12:51:19 +0300

    Fix SB effect

--- effects/shipBonusAF1TorpedoFlightTime.py
+++ effects/shipBonusAF1TorpedoFlightTime.py
@@ -3,5 +3,5 @@
 type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Amarr Frigate").level
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("Torpedoes"),
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.requiresSkill("Torpedoes"),
                                     "explosionDelay", ship.getModifiedItemAttr("shipBonusAF") * level)
