14dfea4a26507e3123418a589a4475811088b040
DarkPhoenix <phoenix@mail.ru>
2010-11-07 18:08:44 +0300

    Add industrial ships tests

--- effects/shipBonusVelocityGI.py
+++ effects/shipBonusVelocityGI.py
@@ -5,6 +5,7 @@
 type = "passive"
 def handler(fit, ship, context):
     level = fit.character.getSkill("Gallente Industrial").level
+    # TODO: investigate if we can live without such ifs or hardcoding
     # Viator doesn't have GI bonus
     if "shipBonusGI" in fit.ship.item.attributes:
         bonusAttr = "shipBonusGI"
