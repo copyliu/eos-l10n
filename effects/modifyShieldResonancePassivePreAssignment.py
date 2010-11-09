19dd0710f13da95981ddf4ad726304cb02fdd01a
Corollax <corollax@gmail.com>
2010-11-09 15:55:07 -0600

    Fix tengu defensive subsystems
    Was pulling armor resists for shields.

--- effects/modifyShieldResonancePassivePreAssignment.py
+++ effects/modifyShieldResonancePassivePreAssignment.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, module, context):
     for type in ("Em", "Kinetic", "Thermal", "Explosive"):
         fit.ship.multiplyItemAttr("shield%sDamageResonance" % type,
-                                  module.getModifiedItemAttr("passiveArmor%sDamageResonance" % type))
+                                  module.getModifiedItemAttr("passiveShield%sDamageResonance" % type))
