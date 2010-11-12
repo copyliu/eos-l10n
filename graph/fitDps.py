fc3a78cfea64213f6036807f2726323438f73213
cncfanatics <diego.duclos@gmail.com>
2010-11-12 20:48:04 +0100

    Fix a misplaced bracket

--- graph/fitDps.py
+++ graph/fitDps.py
@@ -83,7 +83,7 @@ class FitDpsGraph(Graph):
         turretSigRes = mod.getModifiedItemAttr("optimalSigRadius")
         targetSigRad = data["signatureRadius"]
         targetSigRad = turretSigRes if targetSigRad is None else targetSigRad
-        transversal = cos(data["angle"] * data["velocity"])
+        transversal = cos(data["angle"]) * data["velocity"]
         trackingEq = (((transversal / (distance * tracking)) *
                        (turretSigRes / targetSigRad)) ** 2)
         rangeEq = ((max(0, distance - turretOptimal)) / turretFalloff) ** 2
