d86f5447250eb0dc51d31450e330af9f02682eee
DarkPhoenix <phoenix@mail.ru>
2010-11-23 22:56:00 +0300

    Several fixes to effectUsedBy script

--- utils/effectUsedBy.py
+++ utils/effectUsedBy.py
@@ -61,7 +61,7 @@ usage = "usage: %prog --database=DB [--debug=DEBUG]"
 parser = OptionParser(usage=usage)
 parser.add_option("-d", "--database", help="path to eve cache data dump in \
 sqlite format, default pyfa database path is used if none specified",
-type="string", default=os.path.join("~", ".pyfa","eve.db"))
+type="string", default=os.path.join("..", "..","staticdata","eve.db"))
 parser.add_option("-e", "--effects", help="explicit comma-separated list of \
 effects to process", type="string", default="")
 parser.add_option("-u", "--debug", help="debug level, 0 by default",
@@ -464,7 +464,7 @@ for effect_name in effect_list:
 
     stopdebugprints = False
     if DEBUG_LEVEL >= 1:
-        print("\nEffect:", file_name)
+        print("\nEffect:", effect_name)
         print("Total items affected: {0}".format(pereffect_totalaffected))
 
     # Stage 2.2
@@ -1032,7 +1032,7 @@ inner score: {5:.3})"
     # If effect isn't used, write it to file and to terminal
     else:
         commentlines = ["#Not used by any item"]
-        print("Warning: effect file " + file_name +
+        print("Warning: effect file " + effect_name +
               " is not used by any item")
     # Combine "used by" comment lines and actual effect lines
     outputlines = commentlines + effectLines
