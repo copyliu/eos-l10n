80589205a083c6ba7e137235d8be97d07dbfe281
DarkPhoenix <phoenix@mail.ru>
2010-11-29 23:07:27 +0300

    Remove effects which were removed from release version of db

--- effects/carrierCaldariBomberDroneOwnDmg2.py
+++
@@ -1,7 +0,0 @@
-#Used by:
-#Ship: Revenant
-type = "passive"
-def handler(fit, ship, context):
-    level = fit.character.getSkill("Caldari Carrier").level
-    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Fighter Bombers"),
-                                 "damageMultiplier", ship.getModifiedItemAttr("carrierCaldariBonus2") * level)
--- effects/shipJumpRangePirateSupercarrier.py
+++
@@ -1,5 +0,0 @@
-#Used by:
-#Ship: Revenant
-type = "passive"
-def handler(fit, ship, context):
-    fit.ship.boostItemAttr("jumpDriveRange", ship.getModifiedItemAttr("shipBonusPirateFaction"))
