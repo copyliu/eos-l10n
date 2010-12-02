3d8381d01c9b81962aee0f14002b77302f821ea0
DarkPhoenix <phoenix@mail.ru>
2010-12-03 01:30:08 +0300

    Fix crystal effect

--- effects/shieldBoostAmplifierPassive.py
+++ effects/shieldBoostAmplifierPassive.py
@@ -2,5 +2,5 @@
 #Implants named like: Crystal (12 of 12)
 type = "passive"
 def handler(fit, container, context):
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Shield Booster",
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Operation"),
                                   "shieldBonus", container.getModifiedItemAttr("shieldBoostMultiplier"))
