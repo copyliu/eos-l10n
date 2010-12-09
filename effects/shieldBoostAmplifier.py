63c34456946d77d6c4bc161ec38b89c650241be1
Corollax <corollax@gmail.com>
2010-12-08 19:30:21 -0600

    Fix capacitor power relays
    The shield boost penalty was not being properly stacking penalized. This affects primarily chimeras, which are willing to accept the penalty to shield boost amount, since it improves their remote reps.
    
    Near as I can tell, all items with this effect SHOULD be penalized. If the change is too broad, we can revert it.

--- effects/shieldBoostAmplifier.py
+++ effects/shieldBoostAmplifier.py
@@ -5,4 +5,4 @@ type = "passive"
 def handler(fit, module, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Operation") or mod.item.requiresSkill("Capital Shield Operation"),
                                   "shieldBonus", module.getModifiedItemAttr("shieldBoostMultiplier"),
-                                  stackingPenalties = module.item.group.name == "Shield Boost Amplifier")
+                                  stackingPenalties = True)
