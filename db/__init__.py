2e17cf2590949d4196e1d9e5e0982c012b925042
Xreix <raroldan@gmail.com>
2010-12-09 12:53:02 +0100

    Added getCharactersForUser to db import

--- db/__init__.py
+++ db/__init__.py
@@ -56,7 +56,7 @@ from eos.db.gamedata.queries import getItem, searchItems, getVariations, getItem
                                     getMarketGroup, getGroup, getCategory, getAttributeInfo, getMetaData
 from eos.db.saveddata.queries import getUser, getCharacter, getFit, getFitsWithShip, searchFits, \
                                      getCharacterList, getPrice, getDamagePatternList, getDamagePattern, \
-                                     getFitList, getFleetList, getFleet, save, remove, commit, add
+                                     getFitList, getFleetList, getFleet, save, remove, commit, add, getCharactersForUser
 
 #If using in memory saveddata, you'll want to reflect it so the data structure is good.
 if config.saveddata_connectionstring == "sqlite:///:memory:":
