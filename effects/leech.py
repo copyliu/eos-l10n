b2d6d29811bf2ab10f9f6fb50ac31eafcb4ebaec
cncfanatics <diego.duclos@gmail.com>
2010-12-29 10:28:55 +0100

    Fix a nasty bug regarding nosferatus letting a calculated value be affected by skills

--- effects/leech.py
+++ effects/leech.py
@@ -1,10 +1,11 @@
 #Used by:
 #Modules from group: Energy Vampire (52 of 52)
 type = "active", "projected"
+runTime = "late"
 def handler(fit, module, context):
     amount = module.getModifiedItemAttr("powerTransferAmount")
     time = module.getModifiedItemAttr("duration") / 1000.0
     if "projected" in context and fit.ship.getModifiedItemAttr("disallowOffensiveModifiers") != 1:
         fit.addDrain(time, amount, 0)
     elif "module" in context:
-        module.itemModifiedAttributes["capacitorNeed"] = -amount
+        module.itemModifiedAttributes.force("capacitorNeed", -amount)
