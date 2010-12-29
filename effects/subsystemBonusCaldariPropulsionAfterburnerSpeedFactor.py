d52314ed271f99ad09ad6b1c7dd46a063a062408
Corollax <corollax@gmail.com>
2010-12-29 17:56:50 -0600

    Fix a spacing issue
    Cuz DarkPhoenix will be mad =P

--- effects/subsystemBonusCaldariPropulsionAfterburnerSpeedFactor.py
+++ effects/subsystemBonusCaldariPropulsionAfterburnerSpeedFactor.py
@@ -4,4 +4,4 @@ type = "passive"
 def handler(fit, module, context):
     level = fit.character.getSkill("Caldari Propulsion Systems").level
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Afterburner"),
-                                    "speedFactor", module.getModifiedItemAttr("subsystemBonusCaldariPropulsion") * level)
+                                  "speedFactor", module.getModifiedItemAttr("subsystemBonusCaldariPropulsion") * level)
