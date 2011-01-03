baca8dd03c005f669026fa24616d35cd59e7d031
DarkPhoenix <phoenix@mail.ru>
2011-01-03 19:37:45 +0300

    Moved some tests around to the entities they actually belong to

--- tests/typeTests/ships/carriers/nidhoggur.py
+++ tests/typeTests/ships/carriers/nidhoggur.py
@@ -242,8 +242,8 @@ class Test(TestBase):
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
 
-    # Hidden bonus:
     # 99% reduction in CPU need for Drone Control Units
+    # Moved from Drone Control Unit module tests, not listed as bonus of ship
 
     def test_static_cpu_moduleDroneControlUnit(self):
         self.buildTested = 0
@@ -268,6 +268,24 @@ class Test(TestBase):
         fIngame = 3.0
         iEos = self.getShipAttr(attr, ship=ship_other)
         fEos = self.getShipAttr(attr, ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    # Advanced Spaceship Command Skill Bonus:
+    # 5% Bonus to the agility of ship per skill level
+    # Moved from Advanced Spaceship Command skill tests, not listed as bonus of ship
+
+    def test_advancedSpaceshipCommand_agility_ship(self):
+        self.buildTested = 0
+        attr = "agility"
+        skill = "Advanced Spaceship Command"
+        iLvl = 1
+        iIngame = 0.95
+        fLvl = 4
+        fIngame = 0.8
+        iEos = self.getShipAttr(attr, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getShipAttr(attr, skill=(skill, fLvl), ship=self.ship)
         dIngame = fIngame / iIngame
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
