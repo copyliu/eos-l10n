784c495c6ae7413d754ddb02ccebd291b089e627
DarkPhoenix <phoenix@mail.ru>
2011-01-04 22:32:31 +0300

    Add tests for offensive legion subs
    One of them still broken due to misbehaving force support  (see ticket #365)

--- effects/shieldBoostAmplifierPassive.py
+++ effects/shieldBoostAmplifierPassive.py
@@ -1,5 +1,5 @@
 #Used by:
-#Implants named like: Crystal (12 of 12)
+#Implants named like: Crystal (10 of 12)
 type = "passive"
 def handler(fit, container, context):
     fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("Shield Operation"),
