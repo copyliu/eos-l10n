b5d7940ff76478a169bb3fb1098ded9591a4c8bd
DarkPhoenix <phoenix@mail.ru>
2011-01-06 00:08:43 +0300

    Add tengu defensive subs tests

--- effects/slotModifier.py
+++ effects/slotModifier.py
@@ -2,6 +2,6 @@
 #Items from category: Subsystem (80 of 80)
 type = "passive"
 def handler(fit, module, context):
-    fit.ship.increaseItemAttr("lowSlots", module.getModifiedItemAttr("lowSlotModifier"))
-    fit.ship.increaseItemAttr("medSlots", module.getModifiedItemAttr("medSlotModifier"))
     fit.ship.increaseItemAttr("hiSlots", module.getModifiedItemAttr("hiSlotModifier"))
+    fit.ship.increaseItemAttr("medSlots", module.getModifiedItemAttr("medSlotModifier"))
+    fit.ship.increaseItemAttr("lowSlots", module.getModifiedItemAttr("lowSlotModifier"))
