b8e5b50b48ce2b82c2cd05229b9c9eb359e4be50
DarkPhoenix <phoenix@mail.ru>
2011-01-06 12:53:30 +0300

    Add loki electronic sub tests

--- effects/subSystemBonusMinmatarElectronicScanProbeStrength.py
+++ effects/subSystemBonusMinmatarElectronicScanProbeStrength.py
@@ -3,5 +3,5 @@
 type = "passive"
 def handler(fit, module, context):
     level = fit.character.getSkill("Minmatar Electronic Systems").level
-    fit.modules.filteredItemBoost(lambda mod: mod.item.group.name == "Scanner Probe",
-                                  "baseSensorStrength", module.getModifiedItemAttr("subsystemBonusMinmatarElectronic") * level)
+    fit.modules.filteredChargeBoost(lambda mod: mod.charge.group.name == "Scanner Probe",
+                                    "baseSensorStrength", module.getModifiedItemAttr("subsystemBonusMinmatarElectronic") * level)
