5ac8a8876830a2a27b735c698b01c95a68ae00ff
DarkPhoenix <phoenix@mail.ru>
2011-01-06 22:52:14 +0300

    Use new method instead of direct assignment where applicable

--- effects/modifyArmorResonancePassivePreAssignment.py
+++ effects/modifyArmorResonancePassivePreAssignment.py
@@ -3,4 +3,4 @@
 type = "passive"
 def handler(fit, module, context):
     for type in ("Em", "Explosive", "Kinetic", "Thermal"):
-        fit.ship.itemModifiedAttributes["armor{0}DamageResonance".format(type)] = module.getModifiedItemAttr("passiveArmor{0}DamageResonance".format(type))
+        fit.ship.preAssignItemAttr("armor{0}DamageResonance".format(type), module.getModifiedItemAttr("passiveArmor{0}DamageResonance".format(type)))
