ffbfaa9d9bcce7e47f6638f7fb5e2d8c8ed520a2
DarkPhoenix <phoenix@mail.ru>
2011-01-06 18:24:33 +0300

    Add proteus offensive tests

--- effects/subsystemBonusGallenteOffensive3DroneDamageMultiplier.py
+++ effects/subsystemBonusGallenteOffensive3DroneDamageMultiplier.py
@@ -2,7 +2,6 @@
 #Subsystem: Proteus Offensive - Drone Synthesis Projector
 type = "passive"
 def handler(fit, module, context):
-    level = fit.character.getSkill("Gallente Engineering Systems").level
-    fit.drones.filteredItemBoost(lambda drone: drone.item.group.name == "Combat Drone",
-                                 "damageMultiplier",
-                                 module.getModifiedItemAttr("subsystemBonusGallenteOffensive3") * level)
+    level = fit.character.getSkill("Gallente Offensive Systems").level
+    fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
+                                 "damageMultiplier", module.getModifiedItemAttr("subsystemBonusGallenteOffensive3") * level)
