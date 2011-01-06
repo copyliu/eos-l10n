6d13e8d348f4ce31ee25989a9f7214b735d53944
DarkPhoenix <phoenix@mail.ru>
2011-01-06 18:42:15 +0300

    Finish t3 sub tests

--- effects/subsystemBonusGallentePropulsionMWDPenalty.py
+++ effects/subsystemBonusGallentePropulsionMWDPenalty.py
@@ -3,5 +3,5 @@
 type = "passive"
 def handler(fit, module, context):
     level = fit.character.getSkill("Gallente Propulsion Systems").level
-    fit.modules.filteredChargeBoost(lambda mod: mod.item.requiresSkill("High Speed Maneuvering"),
-                                    "signatureRadiusBonus", module.getModifiedItemAttr("subsystemBonusGallentePropulsion") * level)
+    fit.modules.filteredItemBoost(lambda mod: mod.item.requiresSkill("High Speed Maneuvering"),
+                                  "signatureRadiusBonus", module.getModifiedItemAttr("subsystemBonusGallentePropulsion") * level)
