10c81a285365c40f5248dd08a6397a3c5e0ffe21
Corollax <corollax@gmail.com>
2011-01-06 23:49:13 -0600

    Fixed a few miscellaneous effects
    Auto targeters now provide the bonus target even when online/inactive.
    Auto targeters retain an active module activation from the new targetHostiles.py file.
    capacitorCapacityMultiply.py now contains mention that it is used by microwarpdrives (to penalize maximum capacitor).

--- effects/capacitorCapacityMultiply.py
+++ effects/capacitorCapacityMultiply.py
@@ -6,6 +6,9 @@
 #Modules from group: Reactor Control Unit (28 of 28)
 #Modules from group: Shield Flux Coil (11 of 11)
 #Modules from group: Shield Power Relay (11 of 11)
+#Variations of module: 100MN MicroWarpdrive I (24 of 24)
+#Variations of module: 10MN MicroWarpdrive I (14 of 14)
+#Variations of module: 1MN MicroWarpdrive I (15 of 15)
 type = "passive"
 def handler(fit, module, context):
     fit.ship.multiplyItemAttr("capacitorCapacity", module.getModifiedItemAttr("capacitorCapacityMultiplier"))
