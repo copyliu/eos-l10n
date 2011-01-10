96191cc89af2a975eb066a5880febcd17fe0fe57
DarkPhoenix <phoenix@mail.ru>
2011-01-10 15:14:23 +0300

    Re-run script
    Bug wasn't a big deal, fortunately

--- effects/overloadRofBonus.py
+++ effects/overloadRofBonus.py
@@ -1,10 +1,12 @@
 #Used by:
 #Modules from group: Energy Weapon (99 of 181)
 #Modules from group: Hybrid Weapon (109 of 197)
+#Modules from group: Missile Launcher Citadel (2 of 2)
+#Modules from group: Missile Launcher Cruise (20 of 20)
 #Modules from group: Missile Launcher Rocket (14 of 14)
 #Modules from group: Projectile Weapon (59 of 141)
+#Modules named like: Missile Bay (8 of 8)
 #Modules named like: Missile Launcher (77 of 78)
-#Items from market group: Ship Equipment > Turrets & Bays > Missile Launchers (44 of 44)
 type = "overheat"
 def handler(fit, module, context):
     module.boostItemAttr("speed", module.getModifiedItemAttr("overloadRofBonus"))
