4b2ff48cf041461fbb3c7ebd55e0238a5e4dab0a
DarkPhoenix <phoenix@mail.ru>
2011-01-16 15:55:02 +0300

    Autostack drones on eft import

--- saveddata/fit.py
+++ saveddata/fit.py
@@ -114,6 +114,7 @@ class Fit(object):
         except:
             return
 
+        droneMap = {}
         for i in range(1, len(lines)):
             line = lines[i]
             setOffline = line.endswith(offineSuffix)
@@ -136,9 +137,9 @@ class Fit(object):
 
             if item.category.name == "Drone":
                 droneAmount = int(droneAmount) if droneAmount is not None else 1
-                d = Drone(item)
-                d.amount = droneAmount
-                fit.drones.append(d)
+                if not modName in droneMap:
+                    droneMap[modName] = 0
+                droneMap[modName] += droneAmount
             else:
                 m = Module(item)
                 if ammoName:
@@ -150,6 +151,11 @@ class Fit(object):
                     m.state = State.ACTIVE
 
                 fit.modules.append(m)
+
+        for droneName in droneMap:
+            d = Drone(db.getItem(droneName))
+            d.amount = droneMap[droneName]
+            fit.drones.append(d)
 
         return fit
 
