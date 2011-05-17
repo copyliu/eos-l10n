3338704c89a0e4c58440430cb3ff444519b1e0ee
Juozas Rimša <juozas.rimsa@gmail.com>
2011-05-17 13:33:01 +0300

    add stacking penalty to Drone Scope Chip

--- effects/droneMaxRangeBonus.py
+++ effects/droneMaxRangeBonus.py
@@ -4,5 +4,8 @@
 type = "passive"
 def handler(fit, container, context):
     level = container.level if "skill" in context else 1
+    stacking = False if "skill" in context else True
     fit.drones.filteredItemBoost(lambda drone: drone.item.requiresSkill("Drones"),
-                                 "maxRange", container.getModifiedItemAttr("rangeSkillBonus") * level)
+                                 "maxRange",
+                                 container.getModifiedItemAttr("rangeSkillBonus") * level,
+                                 stackingPenalties = stacking)
