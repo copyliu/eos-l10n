f5aba9ee2ac006c3337d360c97c5a1ae8985c67b
DarkPhoenix <phoenix@mail.ru>
2011-09-13 01:05:29 +0400

    Update tests

--- tests/typeTests/ships/commandShips/nighthawk.py
+++ tests/typeTests/ships/commandShips/nighthawk.py
@@ -195,6 +195,112 @@ class Test(TestBase):
 
     # Command Ships Skill Bonus:
     # 5% bonus to heavy missile Kinetic damage per level
+    # Actually this bonus affects all missiles
+
+    def test_commandShips_kineticDamage_chargeMissileRocket(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Thorn Rocket"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileRocketAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Thorn Javelin Rocket"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileLight(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Bloodclaw Light Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileLightAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Bloodclaw Fury Light Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileLightFof(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Serpent F.O.F. Light Missile I"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileAssault(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Terror Assault Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileAssaultAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Terror Rage Assault Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
 
     def test_commandShips_kineticDamage_chargeMissileHeavy(self):
         self.buildTested = 0
@@ -241,10 +347,130 @@ class Test(TestBase):
         dEos = fEos / iEos
         self.assertAlmostEquals(dEos, dIngame)
 
+    def test_commandShips_kineticDamage_chargeMissileTorpedo(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Juggernaut Torpedo"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileTorpedoAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Juggernaut Javelin Torpedo"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileCruise(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Wrath Cruise Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileCruiseAdvanced(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Wrath Fury Cruise Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileCruiseFof(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Dragon F.O.F. Cruise Missile I"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileCitadelTorpedo(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Rift Citadel Torpedo"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileCitadelCruise(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Rajas Citadel Cruise Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.05
+        fLvl = 4
+        fIngame = 1.2
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
+    def test_commandShips_kineticDamage_chargeMissileLightNoSkillrqMissileOp(self):
+        self.buildTested = 0
+        attr = "kineticDamage"
+        item = "Civilian Bloodclaw Light Missile"
+        skill = "Command Ships"
+        iLvl = 1
+        iIngame = 1.0
+        fLvl = 4
+        fIngame = 1.0
+        iEos = self.getItemAttr(attr, item, skill=(skill, iLvl), ship=self.ship)
+        fEos = self.getItemAttr(attr, item, skill=(skill, fLvl), ship=self.ship)
+        dIngame = fIngame / iIngame
+        dEos = fEos / iEos
+        self.assertAlmostEquals(dEos, dIngame)
+
     def test_commandShips_kineticDamage_chargeOther(self):
         self.buildTested = 0
         attr = "kineticDamage"
-        item = "Terror Assault Missile"
+        item = "Hail M"
         skill = "Command Ships"
         iLvl = 1
         iIngame = 1.0
