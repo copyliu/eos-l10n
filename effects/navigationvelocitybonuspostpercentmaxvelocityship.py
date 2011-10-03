01fcf5f0b0f18dfec6fca6503119ada339a8b0a9
DarkPhoenix <phoenix@mail.ru>
2011-10-04 02:14:00 +0400

    Quafe isn't stacking penalized

--- effects/navigationvelocitybonuspostpercentmaxvelocityship.py
+++ effects/navigationvelocitybonuspostpercentmaxvelocityship.py
@@ -8,4 +8,4 @@ def handler(fit, container, context):
     level = container.level if "skill" in context else 1
     amount = container.getModifiedItemAttr("velocityBonus") or 0
     fit.ship.boostItemAttr("maxVelocity", amount * level,
-                           stackingPenalties = "skill" not in context and "implant" not in context)
+                           stackingPenalties = "skill" not in context and "implant" not in context and "booster" not in context)
