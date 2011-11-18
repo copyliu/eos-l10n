3870eeedfe81891bcea313f05b67fac9f2cf13f3
DarkPhoenix <phoenix@mail.ru>
2011-11-18 19:42:56 +0400

    Add getter of custom reload time from module
    Still uses hardcoded 10s reload time, but we can live with it in old eos

--- effects/projectilefired.py
+++ effects/projectilefired.py
@@ -3,5 +3,9 @@
 # Modules from group: Projectile Weapon (143 of 143)
 type = 'active'
 def handler(fit, module, context):
-    # Set reload time to 10 seconds
-    module.reloadTime = 10000
+    rt = module.getModifiedItemAttr("reloadTime")
+    if not rt:
+        # Set reload time to 10 seconds
+        module.reloadTime = 10000
+    else:
+        module.reloadTime = rt
