80da2979d1584698bb382624a6f040a23dae6ddf
DarkPhoenix <phoenix@mail.ru>
2011-11-28 20:12:27 +0400

    Put back t2 missile penalty effects, ccp decided to not remove them

---
+++ effects/ammoinfluenceshipvelocity.py
@@ -0,0 +1,6 @@
+# Used by:
+# Charges named like: Javelin (12 of 15)
+# Charges named like: Precision Missile (12 of 12)
+type = "passive"
+def handler(fit, module, context):
+    fit.ship.multiplyItemAttr("maxVelocity", module.getModifiedChargeAttr("maxVelocityBonus"))
