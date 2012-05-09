260c78a2949b8a7a6a8b930e69085233da2fa6f7
DarkPhoenix <phoenix@mail.ru>
2012-05-09 11:06:13 +0400

    Fix cap boosters

--- effects/ammoinfluencecapneed.py
+++ effects/ammoinfluencecapneed.py
@@ -2,4 +2,8 @@
 # Items from category: Charge (453 of 816)
 type = "passive"
 def handler(fit, module, context):
-    module.boostItemAttr("capacitorNeed", module.getModifiedChargeAttr("capNeedBonus") or 0)
+    # Dirty hack to work around cap charges setting cap booster
+    # injection amount to zero
+    rawAttr = module.item.getAttribute("capacitorNeed")
+    if rawAttr is not None and rawAttr >= 0:
+        module.boostItemAttr("capacitorNeed", module.getModifiedChargeAttr("capNeedBonus") or 0)
